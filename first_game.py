743c62bddf4a8646176c6accd5a0543b7c1a5586
Daniel <kronkowski.daniel@gmail.com>
2023-02-12 18:02:55 +0100

    collision and colors

--- first_game.py
+++ first_game.py
@@ -8,8 +8,10 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render("My game", False, 'Black')
+ground_surf = pygame.image.load('graphics/ground.png').convert()
+
+score_surf = test_font.render("My game", False, (64,64,64))
+score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600,300))
@@ -17,20 +19,36 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80,300))
 
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos):
+                print("collision")
 
-    screen.blit(sky_surface,(0,0)) #umieszczanie warty na wyświetlanum oknie 
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface, (350, 50))
+    screen.blit(sky_surface,(0,0)) #umieszczanie warstwy na wyświetlanym oknie 
+    screen.blit(ground_surf,(0,300))
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+    
+    screen.blit(score_surf, score_rect)
+
     snail_rect.x -=4
     if snail_rect.right < 0:
         snail_rect.left = 800
+
     screen.blit(snail_surf,snail_rect)
     screen.blit(player_surf, player_rect)
 
+    #if player_rect.colliderect(snail_rect):
+    
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint((mouse_pos)):
+    #    print('collision')
+    
     pygame.display.update()
     clock.tick(60)
