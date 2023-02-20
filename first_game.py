49e18016108eec59503731c7c1c840dc249956a6
Daniel <kronkowski.daniel@gmail.com>
2023-02-20 21:39:50 +0100

    improve logic of the game

--- first_game.py
+++ first_game.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = int(pygame.time.get_ticks()/1000) - start_time # Get time
@@ -7,6 +8,26 @@ def display_score():
     score_rect = score_surf.get_rect(center=(400,50))
     screen.blit(score_surf,score_rect)
     return current_time
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom ==300: screen.blit(snail_surf,obstacle_rect)
+            else: screen.blit(fly_surf,obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x >-100]
+
+        return obstacle_list
+
+    else: return []
+
+def collision(player,obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True 
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -20,8 +41,14 @@ score = 0
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surf = pygame.image.load('graphics/ground.png').convert()
 
+# Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(bottomright = (600,300))
+#snail_rect = snail_surf.get_rect(bottomright = (600,300))
+
+fly_surf = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+
+
+obstacle_rect_list = []
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80,300))
@@ -37,6 +64,10 @@ game_name_rect = game_name.get_rect(center = (400,80))
 
 game_message = test_font.render('Press space to run', False, (111,196,169))
 game_message_rect = game_message.get_rect(center = (400,330))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,1550)
 
 while True:
     for event in pygame.event.get():
@@ -54,9 +85,15 @@ while True:
         
         else: 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                snail_rect.x = 800
+                #snail_rect.x = 800
                 game_active = True
                 start_time = int(pygame.time.get_ticks()/1000)
+
+        if event.type == obstacle_timer and game_active:
+            if randint(0,2):
+                obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900,1100),300)))
+            else:
+                obstacle_rect_list.append(fly_surf.get_rect(bottomright = (randint(900,1100),210)))
 
     if game_active:
         screen.blit(sky_surface,(0,0)) #umieszczanie warstwy na wyświetlanym oknie 
@@ -66,11 +103,11 @@ while True:
         #screen.blit(score_surf, score_rect)
         score = display_score()
 
-        snail_rect.x -=4
-        if snail_rect.right < 0:
-            snail_rect.left = 800
+        #snail_rect.x -=4
+        #if snail_rect.right < 0:
+        #    snail_rect.left = 800
 
-        screen.blit(snail_surf,snail_rect)
+        #screen.blit(snail_surf,snail_rect)
 
         # Player
         player_gravity +=1
@@ -78,14 +115,22 @@ while True:
         if player_rect.bottom >= 300: player_rect.bottom = 300
         screen.blit(player_surf, player_rect)
 
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # Collisions
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        #if snail_rect.colliderect(player_rect):
+        #    game_active = False
+        game_active = collision(player_rect,obstacle_rect_list)
+
 
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand,player_stand_rect)
-
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
+        
         score_message = test_font.render(f'Your score: {score}', False, (111,196,169))
         score_message_rect = score_message.get_rect(center = (400,330))
 
